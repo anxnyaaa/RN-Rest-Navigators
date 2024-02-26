f801f11dd6c831554565cf1f362d3e25c537ce8e
anaxnyaaa <ananya.pal@iemlabs.com>
2024-02-26 15:01:06 +0530

    drawer navigation added

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Ionicons} from '@expo/vector-icons'
+
+const BottomTab = createBottomTabNavigator();
+
+import WelcomeScreen from './screens/WelcomeScreen'
+import UserScreen from './screens/UserScreen'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <NavigationContainer>
+    <BottomTab.Navigator screenOptions={{
+      headerStyle: {backgroundColor: '#3c0a6b'},
+      headerTintColor: 'white',
+      tabBarActiveTintColor: '#3c0a6b',
+    }}>
+      <BottomTab.Screen name="Welcome" component={WelcomeScreen} options={{
+        tabBarLabel: 'Welcome Screen',
+        tabBarIcon: ({color, size}) => <Ionicons name='home' color={color} size={size}/>
+      }}/>
+      <BottomTab.Screen name="User" component={UserScreen} options={{
+        tabBarLabel: 'User Screen',
+        tabBarIcon: ({color, size}) => <Ionicons name='person' color={color} size={size}/>
+      }}/>
+    </BottomTab.Navigator>
+  </NavigationContainer>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
